c5475f36984dd85184a214a9f692e6e6f39b6eef
kkannen <kristamkannen@gmail.com>
2018-03-15 13:44:38 -0500

    added files -- ahh how I love a clean, new repository

---
+++ portfolio/src/portfolio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './portfolio.css';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <div className="portfolio">
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
