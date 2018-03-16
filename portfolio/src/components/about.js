c1a310a5cb9c548ae682f2cc65c5900a71b12c4f
kkannen <kristamkannen@gmail.com>
2018-03-16 11:08:44 -0500

    first changes

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -6,7 +6,18 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>about</h1>
+        <div className = 'bio'>
+          <h2>Me and stuff</h2>
+          <p>Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.</p>
+        </div>
+        <div className = 'myLinks'>
+          <h2>Where to find me</h2>
+          <p className = 'links'>titter</p>
+          <p className = 'links'>codepen</p>
+          <p className = 'links'>github</p>
+          <p className = 'links'>google or something</p>
+
+        </div>
       </div>
     );
   }
