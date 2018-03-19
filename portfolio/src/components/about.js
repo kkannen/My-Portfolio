12d9be7232d6b475a4af402b3d66d1fcc0f8a1bc
kkannen <kristamkannen@gmail.com>
2018-03-19 18:37:45 -0500

    added more styling

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -20,6 +20,15 @@ class About extends Component {
           <h2>A LITTLE ABOUT ME:</h2><hr/>
           <p className = 'blurb'>Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.</p>
         </div>
+        <div className = 'mySkills'>
+          <h2>My Skillz</h2><hr/>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>Bootstrap</p>
+          <p>Javascript</p>
+          <p>jQuery</p>
+          <p>React</p>
+        </div>
         <div className = 'myLinks'>
           <h2>Where to find me</h2><hr/>
           {this.state.socialLinks.map((itemArr) => {
--- portfolio/src/components/footer.js
+++ portfolio/src/components/footer.js
@@ -3,13 +3,21 @@ import '../portfolio.css';
 import '../styleSheets/footer.css'
 
 class Footer extends Component {
+  state = {
+    icons: ['https://github.com/kkannen/web-workbook/blob/gh-pages/07week/Checkpoint02/img/homeDark.png?raw=true', 'https://github.com/kkannen/web-workbook/blob/gh-pages/07week/Checkpoint02/img/githubDark.png?raw=true', 'https://github.com/kkannen/web-workbook/blob/gh-pages/07week/Checkpoint02/img/codepenDark.png?raw=true', 'https://github.com/kkannen/web-workbook/blob/gh-pages/07week/Checkpoint02/img/twitterDark.png?raw=true', 'https://github.com/kkannen/web-workbook/blob/gh-pages/07week/Checkpoint02/img/linkedinDark.png?raw=true']
+  }
+
   render() {
     return (
       <div className="footer">
-        <div className = 'footIcon'></div>
-        <div className = 'footIcon'></div>
-        <div className = 'footIcon'></div>
-        <div className = 'footIcon'></div>
+        {this.state.icons.map((icon, l) => {
+            return (
+              <div className = 'iconContainer'>
+                <div className = 'lilDot'></div>
+                <img key = {l} className = 'footIcon' src = {icon}/>
+              </div>
+            )
+        })}
       </div>
     );
   }
